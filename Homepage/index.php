05b312030a530cf7d3de2e3390db173901db8283
Ben Lapidus <blapidus@u.rochester.edu>
2020-05-22 01:32:48 -0400

    Website start
    Created outline for landing page. Includes title text and description and beginning of flex sections.

---
+++ Homepage/index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<head> 
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap"> 
+</head>
+
+<body>
+
+	<header class="head-content">
+		<div class="center-wrapper">
+			<div class="title-wrapper">
+				<h1>Taste Quiz</h1>
+			</div>
+			<div class="description-wrapper">
+				<p>Friend seem to know your music taste better than you? Put it to the test!</p>
+			</div>
+
+			<div class="head-b-wrapper">
+				<a class="head-b-generate-quiz" href="#">Generate Quiz</a>
+			</div>
+		</div>
+		<div class="head-img-wrapper">
+			<img id="music-note-1" src="img/music-notes.png">
+			<img id="music-note-2" src="img/papers-icon.png">
+			<img id="music-note-3" src="img/music-notes.png">
+		</div>
+	</header>
+	<section>
+
+		<div class="flex-container">
+			<div class="flex-child right">
+				<p></p>
+			</div>
+			<div class="flex-child left">
+				<p class="pastel-body-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua.</p>
+			</div>
+		</div>
+	</section>
+
+	<?php
+	include "js/toggle-nav.php";
+	include "js/toggle-x.php";
+	?>
+</body>
+</html>
