64e865c5c1c63131fbad22069ba3ac5f644e16a8
Ben Lapidus <blapidus@u.rochester.edu>
2020-05-24 20:06:58 -0400

    Website gradient update

--- Homepage/index.php
+++ Homepage/index.php
@@ -6,46 +6,45 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/nav.css">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap"> 
 </head>
 
+<nav>
+	<ul>
+		<li>
+			<a href="#"><img src="img/logo.png"></a>
+		</li>
+		<li>
+			<a class="top-sign-in" href="#">Sign in</a>
+		</li>
+	</ul>
+</nav>
+
 <body>
-
-	<header class="head-content">
-		<div class="center-wrapper">
-			<div class="title-wrapper">
-				<h1>Taste Quiz</h1>
-			</div>
-			<div class="description-wrapper">
-				<p>Friend seem to know your music taste better than you? Put it to the test!</p>
-			</div>
-
-			<div class="head-b-wrapper">
-				<a class="head-b-generate-quiz" href="#">Generate Quiz</a>
-			</div>
-		</div>
-		<div class="head-img-wrapper">
-			<img id="music-note-1" src="img/music-notes.png">
-			<img id="music-note-2" src="img/papers-icon.png">
-			<img id="music-note-3" src="img/music-notes.png">
-		</div>
-	</header>
 	<section>
+		<div class="center-wrapper">
+			<div class="flex-container">
+				<div class="flex-child right">
+					<img src="img/demo-1.png">
+				</div>
+				<div class="flex-child left" id="wcsa-section">
+					<div class="title-wrapper">
+						<h1>Taste Quiz</h1>
+					</div>
+					<div class="description-wrapper">
+						<p>Friend seem to know your music taste better than you? Put it to the test!</p>
+					</div>
 
-		<div class="flex-container">
-			<div class="flex-child right">
-				<p></p>
-			</div>
-			<div class="flex-child left">
-				<p class="pastel-body-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua.</p>
+					<div class="head-b-wrapper">
+						<a class="head-b-generate-quiz" id="button-main" href="#">Sign in with Spotify</a>
+					</div>
+
+				</div>
 			</div>
 		</div>
-	</section>
+	</div>
 
-	<?php
-	include "js/toggle-nav.php";
-	include "js/toggle-x.php";
-	?>
+</section>
 </body>
 </html>
