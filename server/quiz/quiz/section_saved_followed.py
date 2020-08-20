12dd394adafc47840b7b59d0fb63946386e3306d
Cassius Close <CassiusClose@gmail.com>
2020-08-20 12:28:09 -0400

    Added saved&followed question section, tested it

---
+++ server/quiz/quiz/section_saved_followed.py
@@ -0,0 +1,146 @@
+import random
+from collections import Counter
+
+from quiz import spotify
+from quiz.models import *
+from .utils import random_from_list
+
+
+def question_saved_albums(quiz, user_data):
+    # Get the user's top artists from the last 6 months
+    saved_albums = user_data.saved_albums()
+
+    if not saved_albums:
+        return None
+
+    # If there are fewer than 4 saved albums, adjust the maximum possible correct answers
+    correct_max = 4
+    if len(saved_albums) < 4:
+        correct_max = len(saved_albums)
+
+    # Choose the correct choices from the saved albums
+    num_of_correct = random.randint(1, correct_max)
+    correct_choices = random_from_list(saved_albums, num_of_correct) 
+    
+    # Determine number of incorrect choices
+    num_of_incorrect = 4-num_of_correct
+    incorrect_choices = []
+    if num_of_incorrect > 0:
+        top_track_albums = strip_albums_from_tracks(user_data.top_tracks('long_term'))
+        incorrect_choices = choose_items_not_in_list(top_track_albums, saved_albums, num_of_incorrect)
+
+        if not incorrect_choices:
+            return None
+
+
+    # Create the database items at the end, once all the data has been
+    # successfully assembled.
+    question = MultipleChoiceQuestion.objects.create(quiz=quiz, text="Which of these albums does the user have saved to their library")
+
+    Choice.create_album_choices(question, correct_choices, answer=True)
+    Choice.create_album_choices(question, incorrect_choices)
+
+    return question
+
+
+
+def question_saved_tracks(quiz, user_data):
+    # Get the user's top artists from the last 6 months
+    saved_tracks = user_data.saved_tracks()
+
+    if not saved_tracks:
+        return None
+
+    # If there are fewer than 4 saved tracks, adjust the max number of correct answers
+    correct_max = 4
+    if len(saved_tracks) < 4:
+        correct_max = len(saved_tracks)
+
+    # Choose correct answers from the saved tracks
+    num_of_correct = random.randint(1, correct_max)
+    correct_choices = random_from_list(saved_tracks, num_of_correct) 
+    
+    # Choose incorrect answers
+    num_of_incorrect = 4-num_of_correct
+    incorrect_choices = []
+    
+    if num_of_incorrect > 0:
+        incorrect_choices = choose_items_not_in_list(user_data.top_tracks('long_term'), saved_tracks, num_of_incorrect)
+        if not incorrect_choices:
+            return None
+
+
+    # Create the database items at the end, once all the data has been
+    # successfully assembled.
+    question = MultipleChoiceQuestion.objects.create(quiz=quiz, text="Which of these tracks does the user have saved to their library")
+
+    Choice.create_track_choices(question, correct_choices, answer=True)
+    Choice.create_track_choices(question, incorrect_choices)
+
+    return question
+
+
+
+
+
+def question_followed_artists(quiz, user_data):
+    # Get the user's top artists from the last 6 months
+    followed_artists = user_data.followed_artists()
+
+    # If 
+    if not followed_artists:
+        return None
+
+    correct_max = 4
+    if len(followed_artists) < 4:
+        correct_max = len(followed_artists)
+
+    num_of_correct = random.randint(1, correct_max)
+    correct_choices = random_from_list(followed_artists, num_of_correct) 
+    
+    num_of_incorrect = 4-num_of_correct
+    incorrect_choices = []
+
+    if num_of_incorrect > 0:
+        incorrect_choices = choose_items_not_in_list(user_data.top_artists('long_term'), followed_artists, num_of_incorrect)
+
+        if not incorrect_choices:
+            return None
+
+    # Create the database items at the end, once all the data has been
+    # successfully assembled.
+    question = MultipleChoiceQuestion.objects.create(quiz=quiz, text="Which of these tracks does the user have saved to their library")
+
+    Choice.create_artist_choices(question, correct_choices, answer=True)
+    Choice.create_artist_choices(question, incorrect_choices)
+
+    return question
+
+
+
+
+
+def choose_items_not_in_list(item_list, excluded_items, num_chosen):
+    chosen = [] 
+
+    items = item_list.copy()
+    for i in range(num_chosen):
+        i = random.randint(0, len(items)-1)
+        while len(items) > 1 and items[i] in excluded_items:
+            del items[i]
+            i = random.randint(0, len(items)-1)
+
+        if items[i] in excluded_items:
+            return None
+
+        chosen.append(items[i])
+
+    return chosen
+        
+
+            
+def strip_albums_from_tracks(tracks):
+    albums = [] 
+    for t in tracks:
+        albums.append(t['album'])
+    return albums
---
+++ server/quiz/tests/quiz/test_section_saved_followed.py
@@ -0,0 +1,430 @@
+from django.test import TransactionTestCase, TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from quiz import spotify
+from quiz.quiz.user_data import UserData
+from quiz.tests.setup_tests import create_authorized_session
+from quiz.models import *
+from quiz.quiz.section_saved_followed import *
+
+class QuestionSavedAlbumsTests(StaticLiveServerTestCase):
+    port = 8000 
+
+    @classmethod 
+    def setUpClass(cls):
+        """
+        These tests only need a user to be logged into a session, so
+        this does it once at class creation. Saves the session data by itself
+        so that each test can have a fresh session with that data.
+        """
+        super(QuestionSavedAlbumsTests, cls).setUpClass()
+        cls.session = create_authorized_session(cls.live_server_url)
+
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        At the end of this class, complete any timers that would delete
+        auth_access_tokens, so that they don't hang up the testing program.
+        """
+        super(QuestionSavedAlbumsTests, cls).tearDownClass()
+        spotify.cleanup_timers()
+
+
+    def test_question_saved_albums(self):
+        """
+        When the user has valid data, question_saved_albums() should return a proper Question
+        about the user's saved albums.
+        """
+        u = UserData(self.session)
+        u._saved_albums = [
+            {'name': 'Country Album 1', 'id': 1, 'artists': [{'name': 'Cash'}]}, {'name': 'Country Album 2', 'id': 2, 'artists': [{'name': 'Ben'}]}, {'name': 'Country Album 3', 'id': 3, 'artists': [{'name': 'Cassius'}]},
+            {'name': 'Country Album 4', 'id': 4, 'artists': [{'name': 'Benjamin'}]},
+            {'name': 'Country Album 5', 'id': 5, 'artists': [{'name': 'James'}]},
+            {'name': 'Country Album 6', 'id': 6, 'artists': [{'name': 'Jim'}]},
+            {'name': 'Country Album 7', 'id': 7, 'artists': [{'name': 'John'}]},
+        ]
+
+        u._top_tracks['long_term'] = [
+            {'album': {'name': 'Rock Album 1', 'id': 8, 'artists': [{'name': 'Lucy'}]}},
+            {'album': {'name': 'Rock Album 2', 'id': 9, 'artists': [{'name': 'Lewis'}]}},
+            {'album': {'name': 'Rock Album 3', 'id': 10, 'artists': [{'name': 'Lucifer'}]}},
+            {'album': {'name': 'Rock Album 1', 'id': 11, 'artists': [{'name': 'Lewd'}]}},
+            {'album': {'name': 'Country Album 3', 'id': 3, 'artists': [{'name': 'Cassius'}]},},
+            {'album': {'name': 'Country Album 4', 'id': 4, 'artists': [{'name': 'Benjamin'}]},},
+            {'album': {'name': 'Country Album 5', 'id': 5, 'artists': [{'name': 'James'}]},},
+            {'album': {'name': 'Country Album 6', 'id': 6, 'artists': [{'name': 'Jim'}]},},
+        ]
+
+        quiz = Quiz.objects.create(user_id='cassius')
+
+        q = question_saved_albums(quiz, u)
+
+        self.assertEquals(q.choices.count(), 4)
+        self.assertGreater(q.answers().count(), 0)
+        self.assertLessEqual(q.answers().count(), 4)
+        self.assertEqual(q.incorrect_answers().count(), 4-q.answers().count())
+
+        for c in q.answers():
+            title = c.primary_text
+            artist = c.secondary_text
+            found = False
+            for a in u._saved_albums:
+                if a['name'] == title and a['artists'][0]['name'] == artist:
+                    found = True
+            self.assertTrue(found)
+        
+        for c in q.incorrect_answers():
+            title = c.primary_text
+            artist = c.secondary_text
+            found = False
+            for t in u._top_tracks['long_term']:
+                if t['album']['name'] == title and t['album']['artists'][0]['name'] == artist:
+                    found = True
+            self.assertTrue(found)
+
+
+    def test_question_saved_albums_only_one_correct_answer(self):
+        """
+        question_saved_albums() should create a question, even if there is only one available
+        album that can be correct, as long as there are enough albums in "top_tracks" to fill
+        the incorrect choices.
+        """
+        u = UserData(self.session)
+        u._saved_albums = [
+            {'name': 'Country Album 1', 'id': 1, 'artists': [{'name': 'Cassius'}]},
+        ]
+
+        u._top_tracks['long_term'] = [
+            {'album': {'name': 'Country Album 2', 'id': 2, 'artists': [{'name': 'Cassius'}]},},
+            {'album': {'name': 'Country Album 3', 'id': 3, 'artists': [{'name': 'Benjamin'}]},},
+            {'album': {'name': 'Country Album 4', 'id': 4, 'artists': [{'name': 'James'}]},},
+        ]
+
+        quiz = Quiz.objects.create(user_id='cassius')
+        q = question_saved_albums(quiz, u)
+
+        self.assertEquals(q.choices.count(), 4)
+        self.assertEqual(q.answers().count(), 1)
+        self.assertEqual(q.incorrect_answers().count(), 3)
+
+        c = q.answers()[0]
+        self.assertEqual(u._saved_albums[0]['name'], c.primary_text)
+        self.assertEqual(u._saved_albums[0]['artists'][0]['name'], c.secondary_text)
+        
+        for c in q.incorrect_answers():
+            title = c.primary_text
+            artist = c.secondary_text
+            found = False
+            for t in u._top_tracks['long_term']:
+                if t['album']['name'] == title and t['album']['artists'][0]['name'] == artist:
+                    found = True
+            self.assertTrue(found)
+
+
+    def test_question_saved_albums_no_valid_incorrect_choices(self):
+        """
+        question_saved_albums() should return None if there are not enough albums to fill
+        correct choices, and if there are not enough albums from "top_tracks" to fill
+        incorrect choices.
+        """
+        u = UserData(self.session)
+        u._saved_albums = [
+            {'name': 'Country Album 1', 'id': 1, 'artists': [{'name': 'Cassius'}]},
+            {'name': 'Country Album 2', 'id': 2, 'artists': [{'name': 'Benjamin'}]},
+            {'name': 'Country Album 3', 'id': 3, 'artists': [{'name': 'James'}]},
+        ]
+
+        u._top_tracks['long_term'] = [
+            {'album': {'name': 'Country Album 1', 'id': 1, 'artists': [{'name': 'Cassius'}]},},
+            {'album': {'name': 'Country Album 2', 'id': 2, 'artists': [{'name': 'Benjamin'}]},},
+            {'album': {'name': 'Country Album 3', 'id': 3, 'artists': [{'name': 'James'}]},},
+        ]
+
+        quiz = Quiz.objects.create(user_id='cassius')
+        q = question_saved_albums(quiz, u)
+
+        self.assertIsNone(q)
+
+
+class QuestionSavedTracksTests(StaticLiveServerTestCase):
+
+    port = 8000 
+
+    @classmethod 
+    def setUpClass(cls):
+        """
+        These tests only need a user to be logged into a session, so
+        this does it once at class creation. Saves the session data by itself
+        so that each test can have a fresh session with that data.
+        """
+        super(QuestionSavedTracksTests, cls).setUpClass()
+         
+        cls.session = create_authorized_session(cls.live_server_url)
+        #cls.refresh_token = session.get(spotify.REFRESH_TOKEN)
+        #cls.auth_access_token = session.get(spotify.AUTH_ACCESS_TOKEN)
+        #cls.user_id = session.get(spotify.USER_ID)
+
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        At the end of this class, complete any timers that would delete
+        auth_access_tokens, so that they don't hang up the testing program.
+        """
+        super(QuestionSavedTracksTests, cls).tearDownClass()
+        spotify.cleanup_timers()
+
+
+    def test_question_saved_tracks(self):
+        """
+        When the user has valid data, question_saved_tracks() should return a proper Question
+        about the user's saved tracks.
+        """
+        u = UserData(self.session)
+        u._saved_tracks = [
+            {'name': 'Country Track 1', 'id': 1, 'artists': [{'name': 'Cash'}]},
+            {'name': 'Country Track 2', 'id': 2, 'artists': [{'name': 'Ben'}]},
+            {'name': 'Country Track 3', 'id': 3, 'artists': [{'name': 'Cassius'}]},
+            {'name': 'Country Track 4', 'id': 4, 'artists': [{'name': 'Benjamin'}]},
+            {'name': 'Country Track 5', 'id': 5, 'artists': [{'name': 'James'}]},
+            {'name': 'Country Track 6', 'id': 6, 'artists': [{'name': 'Jim'}]},
+            {'name': 'Country Track 7', 'id': 7, 'artists': [{'name': 'John'}]},
+        ]
+
+        u._top_tracks['long_term'] = [
+            { 'name': 'Rock Track 1', 'id': 8, 'artists': [{'name': 'Lucy'}]},
+            { 'name': 'Rock Track 2', 'id': 9, 'artists': [{'name': 'Lewis'}]},
+            { 'name': 'Rock Track 3', 'id': 10, 'artists': [{'name': 'Lucifer'}]},
+            { 'name': 'Rock Track 4', 'id': 11, 'artists': [{'name': 'Luny'}]},
+            { 'name': 'Rock Track 5', 'id': 12, 'artists': [{'name': 'Lewd'}]},
+        ]
+
+        quiz = Quiz.objects.create(user_id='cassius')
+
+        q = question_saved_tracks(quiz, u)
+
+        self.assertEquals(q.choices.count(), 4)
+        self.assertGreater(q.answers().count(), 0)
+        self.assertLessEqual(q.answers().count(), 4)
+        self.assertEqual(q.incorrect_answers().count(), 4-q.answers().count())
+
+        for c in q.answers():
+            title = c.primary_text
+            artist = c.secondary_text
+            found = False
+            for t in u._saved_tracks:
+                if t['name'] == title and t['artists'][0]['name'] == artist:
+                    found = True
+            self.assertTrue(found)
+        
+        for c in q.incorrect_answers():
+            title = c.primary_text
+            artist = c.secondary_text
+            found = False
+            for t in u._top_tracks['long_term']:
+                if t['name'] == title and t['artists'][0]['name'] == artist:
+                    found = True
+            self.assertTrue(found)
+
+
+    def test_question_saved_tracks_only_one_correct_answer(self):
+        """
+        question_saved_tracks() should create a question, even if there is only one available
+        track that can be correct, as long as there are enough tracks in "top_tracks" to fill
+        the incorrect choices.
+        """
+        u = UserData(self.session)
+        u._saved_tracks = [
+            {'name': 'Country Track 1', 'id': 1, 'artists': [{'name': 'Cassius'}]},
+        ]
+
+        u._top_tracks['long_term'] = [
+            {'name': 'Country Track 2', 'id': 2, 'artists': [{'name': 'Ben'}]},
+            {'name': 'Country Track 3', 'id': 3, 'artists': [{'name': 'John'}]},
+            {'name': 'Country Track 4', 'id': 4, 'artists': [{'name': 'Jim'}]},
+        ]
+
+        quiz = Quiz.objects.create(user_id='cassius')
+        q = question_saved_tracks(quiz, u)
+
+        self.assertEquals(q.choices.count(), 4)
+        self.assertEqual(q.answers().count(), 1)
+        self.assertEqual(q.incorrect_answers().count(), 3)
+
+        c = q.answers()[0]
+        self.assertEqual(u._saved_tracks[0]['name'], c.primary_text)
+        self.assertEqual(u._saved_tracks[0]['artists'][0]['name'], c.secondary_text)
+        
+        for c in q.incorrect_answers():
+            title = c.primary_text
+            artist = c.secondary_text
+            found = False
+            for t in u._top_tracks['long_term']:
+                if t['name'] == title and t['artists'][0]['name'] == artist:
+                    found = True
+            self.assertTrue(found)
+
+
+    def test_question_saved_tracks_no_valid_incorrect_choices(self):
+        """
+        question_saved_tracks() should return None if there are not enough tracks to fill
+        correct choices, and if there are not enough tracks from "top_tracks" to fill
+        incorrect choices.
+        """
+        u = UserData(self.session)
+        u._saved_tracks = [
+            {'name': 'Country Track 1', 'id': 1, 'artists': [{'name': 'Cassius'}]},
+            {'name': 'Country Track 2', 'id': 2, 'artists': [{'name': 'Benjamin'}]},
+            {'name': 'Country Track 3', 'id': 3, 'artists': [{'name': 'James'}]},
+        ]
+
+        u._top_tracks['long_term'] = [
+            {'name': 'Country Track 1', 'id': 1, 'artists': [{'name': 'Cassius'}]},
+            {'name': 'Country Track 2', 'id': 2, 'artists': [{'name': 'Benjamin'}]},
+            {'name': 'Country Track 3', 'id': 3, 'artists': [{'name': 'James'}]},
+        ]
+
+        quiz = Quiz.objects.create(user_id='cassius')
+        q = question_saved_tracks(quiz, u)
+
+        self.assertIsNone(q)
+
+
+class QuestionFollowedArtistsTests(StaticLiveServerTestCase):
+
+    port = 8000 
+
+    @classmethod 
+    def setUpClass(cls):
+        """
+        These tests only need a user to be logged into a session, so
+        this does it once at class creation. Saves the session data by itself
+        so that each test can have a fresh session with that data.
+        """
+        super(QuestionFollowedArtistsTests, cls).setUpClass()
+         
+        cls.session = create_authorized_session(cls.live_server_url)
+
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        At the end of this class, complete any timers that would delete
+        auth_access_tokens, so that they don't hang up the testing program.
+        """
+        super(QuestionFollowedArtistsTests, cls).tearDownClass()
+        spotify.cleanup_timers()
+
+
+    def test_question_followed_artists(self):
+        """
+        When the user has valid data, question_followed_artists() should return a proper Question
+        about the user's followed artists.
+        """
+        u = UserData(self.session)
+        u._followed_artists = [
+            {'name': 'Cassius'},
+            {'name': 'Cash'},
+            {'name': 'Ben'},
+            {'name': 'Benjamin'},
+            {'name': 'James'},
+            {'name': 'Jimmy'},
+            {'name': 'John'},
+        ]
+
+        u._top_artists['long_term'] = [
+            {'name': 'Lucy'},
+            {'name': 'Lewis'},
+            {'name': 'Lucifer'},
+            {'name': 'Luny'},
+            {'name': 'Lewd'},
+        ]
+
+        quiz = Quiz.objects.create(user_id='cassius')
+
+        q = question_followed_artists(quiz, u)
+
+        self.assertEquals(q.choices.count(), 4)
+        self.assertGreater(q.answers().count(), 0)
+        self.assertLessEqual(q.answers().count(), 4)
+        self.assertEqual(q.incorrect_answers().count(), 4-q.answers().count())
+
+        for c in q.answers():
+            title = c.primary_text
+            found = False
+            for t in u._followed_artists:
+                if t['name'] == title:
+                    found = True
+            self.assertTrue(found)
+        
+        for c in q.incorrect_answers():
+            title = c.primary_text
+            found = False
+            for a in u._top_artists['long_term']:
+                if a['name'] == title:
+                    found = True
+            self.assertTrue(found)
+
+
+    def test_question_followed_artists_only_one_correct_answer(self):
+        """
+        question_followed_artists() should create a question, even if there is only one available
+        artist that can be correct, as long as there are enough artists in "top_artists" to fill
+        the incorrect choices.
+        """
+        u = UserData(self.session)
+        u._followed_artists = [
+            {'name': 'Cassius'},
+        ]
+
+        u._top_artists['long_term'] = [
+            {'name': 'Ben'},
+            {'name': 'John'},
+            {'name': 'Jim'},
+        ]
+
+        quiz = Quiz.objects.create(user_id='cassius')
+        q = question_followed_artists(quiz, u)
+
+        self.assertEquals(q.choices.count(), 4)
+        self.assertEqual(q.answers().count(), 1)
+        self.assertEqual(q.incorrect_answers().count(), 3)
+
+        c = q.answers()[0]
+        self.assertEqual(u._followed_artists[0]['name'], c.primary_text)
+        
+        for c in q.incorrect_answers():
+            title = c.primary_text
+            found = False
+            for a in u._top_artists['long_term']:
+                if a['name'] == title:
+                    found = True
+            self.assertTrue(found)
+
+
+    def test_question_followed_artists_no_valid_incorrect_choices(self):
+        """
+        question_followed_artists() should return None if there are not enough artists to fill
+        correct choices, and if there are not enough artists from "top_artists" to fill
+        incorrect choices.
+        """
+        u = UserData(self.session)
+        u._followed_artists = [
+            {'name': 'Cassius'},
+            {'name': 'Benjamin'},
+            {'name': 'James'},
+        ]
+
+        u._top_artists['long_term'] = [
+            {'name': 'Cassius'},
+            {'name': 'Benjamin'},
+            {'name': 'James'},
+        ]
+
+        quiz = Quiz.objects.create(user_id='cassius')
+        q = question_followed_artists(quiz, u)
+
+        self.assertIsNone(q)
+
+
