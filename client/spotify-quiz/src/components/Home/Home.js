b3a4f4371bf6c425c80b347257933b84ad92e128
Ben Lapidus <benjamin_lapidus@nyeb.uscourts.gov>
2020-11-17 02:24:56 -0500

    Updated homepage

--- client/spotify-quiz/src/components/Home/Home.js
+++ client/spotify-quiz/src/components/Home/Home.js
@@ -1,14 +1,30 @@
 import React from "react";
-import "./Home.css";
+import "./Home.scss";
 
 const Home = (props) => {
   return (
     <div className="Home">
+      <nav>
+        <img className="logo" src={require('./../../assets/rock-on.svg')} />
+        <div className="navSignIn">
+        <a href="#">Sign In</a>
+        </div>
+      </nav>
       <div className="header">
         <h1 className="title">
-          <span className="highlight">spot</span>on
+          <span className="highlight">rock</span>on
         </h1>
-          <h2>Hello</h2>
+        <h2 className="description">
+          Generate and share a personalized quiz to put your music taste to the
+          test. Powered by Spotify.
+        </h2>
+      </div>
+      <div className="sectionBottom">
+        <div className="center">
+          <a href="#" className="heroButton">
+            Generate Quiz
+          </a>
+        </div>
       </div>
     </div>
   );
