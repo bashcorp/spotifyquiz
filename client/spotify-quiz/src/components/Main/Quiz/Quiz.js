3566c3a900e03ccd4cd04312ca5b2ac089f330f5
Ben Lapidus <blapidus@u.rochester.edu>
2021-01-31 23:12:35 -0500

    CSRF Token implementation

--- client/spotify-quiz/src/components/Main/Quiz/Quiz.js
+++ client/spotify-quiz/src/components/Main/Quiz/Quiz.js
@@ -104,10 +104,31 @@ const [questionNumber, setQuestionNumber] = React.useState(0);
     }
   }
 
+  var $ = require('zepto-browserify').$;
+
+var getCookie = function(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = $.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+var csrftoken = getCookie('csrftoken');
+
   function postRequest() {
     var xhr = new XMLHttpRequest();
     xhr.open("POST", '/response/', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader('X-CSRFToken', csrftoken);
     xhr.send(JSON.stringify({
     'quiz_id': quiz.user_id,
     'name': "ben",
