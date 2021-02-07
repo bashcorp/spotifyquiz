63d05752131765645179234926c19163193035b5
Cassius Close <CassiusClose@gmail.com>
2021-02-07 11:16:32 -0500

    Tested the save_response() function

--- server/spoton/quiz/response.py
+++ server/spoton/quiz/response.py
@@ -8,9 +8,27 @@ logger = logging.getLogger(__name__)
 
 
 def save_response(data):
+    """Processes a user's response to the quiz and saves it to the db.
+
+    Processes a user's response to the quiz, loads their answers into
+    Response object, and saves it to the database.
+
+    Parameters
+    ----------
+    data : dict
+        A JSON dictionary of the user's response data. Format specified
+        in coord/response.json (above the server folder). 
+    
+    Returns
+    -------
+    bool
+        True if the response was processed and saved successfully,
+        False otherwise.
+    """
 
     #import pprint; pprint.pprint(data)
 
+    # Find the specified quiz
     quizzes = Quiz.objects.filter(user_id=data.get('quiz_id'))
     if not quizzes:
         logger.error('Processing Response: No quiz was found with id '
@@ -19,14 +37,22 @@ def save_response(data):
 
     quiz = quizzes[0]
 
+
+
+    # Load in general response data and create response object
     name = data.get('name')
+    # emoji
+    # background color
 
     response = Response.objects.create(quiz=quiz, name=name)
 
+
+
+    # Process each question response
     for q in data.get('questions'):
 
+        # Get answer(s) for the specific question
         answers = q.get('answer')
-
         if not answers:
             response.delete()
             logger.error('Processing Response: No answer found for question '
@@ -34,7 +60,11 @@ def save_response(data):
             return False
 
 
+        # If there's a list of answers (even only one), it's a checkbox
+        # question
         if(type(answers) is list):
+
+            # Get the specified question object
             questions = CheckboxQuestion.objects.filter(id=q.get('question_id'))
             if not questions:
                 response.delete()
@@ -42,33 +72,48 @@ def save_response(data):
                         + 'for question ' + str(q) +
                         '. This is an internal error.')
                 return False
-
             question = questions[0]
+
+
 
             qr = CheckboxResponse.objects.create(response=response,
                     question=question)
 
+
+            # Go through each of the answers and load the associated
+            # Choice object
             for a in answers:
                 c = Choice.objects.filter(id=a)
                 if not c:
                     response.delete()
                     logger.error('Processing Response: No Choice object '
-                            + 'found for answer ' + str(a) +
+                            + 'found with id ' + str(a) +
                             '. This is an internal error.')
                     return False
-                qr.choices.add(c[0])
+
+                try:
+                    qr.choices.add(c[0])
+                except ValidationError as e:
+                    response.delete()
+                    logger.error(e)
+                    logger.error('ValidationError raised when adding '
+                            + 'Choice to CheckboxResponse. This is an '
+                            + 'internal error.')
+                    return False
             
+        # Slider question
         else:
+            # Get the specified question object
             questions = SliderQuestion.objects.filter(id=q.get('question_id'))
             if not questions:
                 response.delete()
                 logger.error('Processing Response: No SliderQuestion found '
                         + 'for question ' + str(q) +
-                        '. Are you sure the answers should not be a list. '
+                        '. Are you sure the answers should not be a list? '
                         + 'This is an internal error.')
                 return False
-
             question = questions[0]
+
 
             qr = SliderResponse.objects.create(response=response,
                     question=question, answer=answers)
--- server/spoton/tests/quiz/test_response.py
+++ server/spoton/tests/quiz/test_response.py
@@ -7,36 +7,47 @@ from spoton.quiz import save_response
 
 
 class SaveResponseTests(TransactionTestCase):
-    @classmethod
-    def setUpClass(cls):
+    """
+    save_response() takes a user's response to a Quiz, processes it,
+    and loads the data into database models. This tests that function.
+    """
+
+    def setUp(self):
+        """
+        Create a quiz that the tests can create responses to.
+        """
         Quiz.objects.delete()
 
-        cls.quiz = Quiz.objects.create(user_id='cassius')
-        cls.q1 = CheckboxQuestion.objects.create(quiz=cls.quiz, text="q1")
-        cls.c11 = Choice.objects.create(question=cls.q1, answer=True)
-        cls.c12 = Choice.objects.create(question=cls.q1)
-        cls.c13 = Choice.objects.create(question=cls.q1)
-        cls.c14 = Choice.objects.create(question=cls.q1)
+        self.quiz = Quiz.objects.create(user_id='cassius')
+        self.q1 = CheckboxQuestion.objects.create(quiz=self.quiz, text="q1")
+        self.c11 = Choice.objects.create(question=self.q1, answer=True)
+        self.c12 = Choice.objects.create(question=self.q1)
+        self.c13 = Choice.objects.create(question=self.q1)
+        self.c14 = Choice.objects.create(question=self.q1)
 
-        cls.q2 = CheckboxQuestion.objects.create(quiz=cls.quiz, text="q2",
+        self.q2 = CheckboxQuestion.objects.create(quiz=self.quiz, text="q2",
                 multiselect=True)
-        cls.c21 = Choice.objects.create(question=cls.q2)
-        cls.c22 = Choice.objects.create(question=cls.q2)
-        cls.c23 = Choice.objects.create(question=cls.q2, answer=True)
-        cls.c24 = Choice.objects.create(question=cls.q2)
+        self.c21 = Choice.objects.create(question=self.q2)
+        self.c22 = Choice.objects.create(question=self.q2)
+        self.c23 = Choice.objects.create(question=self.q2, answer=True)
+        self.c24 = Choice.objects.create(question=self.q2)
         
-        cls.q3 = CheckboxQuestion.objects.create(quiz=cls.quiz, text="q3",
+        self.q3 = CheckboxQuestion.objects.create(quiz=self.quiz, text="q3",
                 multiselect=True)
-        cls.c31 = Choice.objects.create(question=cls.q3)
-        cls.c32 = Choice.objects.create(question=cls.q3, answer=True)
-        cls.c33 = Choice.objects.create(question=cls.q3, answer=True)
-        cls.c34 = Choice.objects.create(question=cls.q3)
+        self.c31 = Choice.objects.create(question=self.q3)
+        self.c32 = Choice.objects.create(question=self.q3, answer=True)
+        self.c33 = Choice.objects.create(question=self.q3, answer=True)
+        self.c34 = Choice.objects.create(question=self.q3)
         
-        cls.q4 = SliderQuestion.objects.create(quiz=cls.quiz, text="q4",
+        self.q4 = SliderQuestion.objects.create(quiz=self.quiz, text="q4",
                 slider_min = 0, slider_max = 50, answer = 25)
 
 
     def test_save_response(self):
+        """
+        save_response() takes a user's response to a Quiz, processes it,
+        and loads the data into database models. This tests that function.
+        """
 
         data = {
             'quiz_id': self.quiz.user_id,
@@ -95,28 +106,316 @@ class SaveResponseTests(TransactionTestCase):
 
 
     def test_save_response_bad_quiz_id(self):
+        """
+        save_response() should fail when the quiz id is invalid. This
+        means it should return false and no response object should be
+        saved in the database.
+        """
 
         data = {
             'quiz_id': 'none',
             'name': 'Benjamin',
             'emoji': '😀',
             'background_color': 0x333333,
+            'questions': []
+        }
+         
+        result = save_response(data)
+
+        self.assertFalse(result)
+
+        self.assertEqual(Response.objects.count(), 0)
+        self.assertEqual(QuestionResponse.objects.count(), 0)
+
+
+    def test_save_response_checkbox_no_answer_field(self):
+        """
+        save_response() should fail when there is no answer field in a 
+        question. This means it should return false and no response
+        object should be saved in the database.
+        """
+
+        data = {
+            'quiz_id': self.quiz.user_id,
+            'name': 'Benjamin',
+            'emoji': '😀',
+            'background_color': 0x333333,
             'questions': [
                 {
-                    'question_id': 1,
-                    'answer': [1]
+                    'question_id': self.q1.id,
+                    'answer': [self.c11.id]
                 },
                 {
-                    'question_id': 2,
-                    'answer': [5, 7]
+                    'question_id': self.q2.id,
                 },
                 {
-                    'question_id': 3,
-                    'answer': [11, 12]
+                    'question_id': self.q3.id,
+                    'answer': [self.c33.id, self.c34.id]
+                },
+            ]
+        }
+         
+        result = save_response(data)
+
+        self.assertFalse(result)
+
+        self.assertEqual(Response.objects.count(), 0)
+        self.assertEqual(QuestionResponse.objects.count(), 0)
+
+
+    def test_save_response_checkbox_no_question_id_field(self):
+        """
+        save_response() should fail when there is no question id field
+        in a question. This means it should return false and no
+        response object should be saved in the database.
+        """
+
+        data = {
+            'quiz_id': self.quiz.user_id,
+            'name': 'Benjamin',
+            'emoji': '😀',
+            'background_color': 0x333333,
+            'questions': [
+                {
+                    'question_id': self.q1.id,
+                    'answer': [self.c11.id]
                 },
                 {
-                    'question_id': 4,
+                    'question_id': self.q2.id,
+                    'answer': [self.c23.id]
+                },
+                {
+                    'answer': [self.c33.id, self.c34.id]
+                },
+            ]
+        }
+         
+        result = save_response(data)
+
+        self.assertFalse(result)
+
+        self.assertEqual(Response.objects.count(), 0)
+        self.assertEqual(QuestionResponse.objects.count(), 0)
+
+
+    def test_save_response_slider_no_answer_field(self):
+        """
+        save_response() should fail when there is no answer field in a 
+        question. This means it should return false and no response
+        object should be saved in the database.
+        """
+
+        data = {
+            'quiz_id': self.quiz.user_id,
+            'name': 'Benjamin',
+            'emoji': '😀',
+            'background_color': 0x333333,
+            'questions': [
+                {
+                    'question_id': self.q1.id,
+                    'answer': [self.c11.id]
+                },
+                {
+                    'question_id': self.q2.id,
+                    'answer': [self.c22.id]
+                },
+                {
+                    'question_id': self.q3.id,
+                    'answer': [self.c33.id, self.c34.id]
+                },
+                {
+                    'question_id': self.q4.id
+                }
+            ]
+        }
+         
+        result = save_response(data)
+
+        self.assertFalse(result)
+
+        self.assertEqual(Response.objects.count(), 0)
+        self.assertEqual(QuestionResponse.objects.count(), 0)
+
+
+    def test_save_response_slider_no_question_id_field(self):
+        """
+        save_response() should fail when there is no question id field
+        in a question. This means it should return false and no
+        response object should be saved in the database.
+        """
+
+        data = {
+            'quiz_id': self.quiz.user_id,
+            'name': 'Benjamin',
+            'emoji': '😀',
+            'background_color': 0x333333,
+            'questions': [
+                {
+                    'question_id': self.q1.id,
+                    'answer': [self.c11.id]
+                },
+                {
+                    'question_id': self.q2.id,
+                    'answer': [self.c23.id]
+                },
+                {
+                    'answer': 40
+                },
+            ]
+        }
+         
+        result = save_response(data)
+
+        self.assertFalse(result)
+
+        self.assertEqual(Response.objects.count(), 0)
+        self.assertEqual(QuestionResponse.objects.count(), 0)
+
+
+    def test_save_response_checkbox_bad_choice_id(self):
+        """
+        save_response() should fail when one of the choice ids is
+        invalid.  in a question. This means it should return false and
+        no response object should be saved in the database.
+        """
+
+        data = {
+            'quiz_id': self.quiz.user_id,
+            'name': 'Benjamin',
+            'emoji': '😀',
+            'background_color': 0x333333,
+            'questions': [
+                {
+                    'question_id': self.q1.id,
+                    'answer': [self.c11.id]
+                },
+                {
+                    'question_id': self.q2.id,
+                    'answer': [self.c21.id, -24]
+                },
+                {
+                    'question_id': self.q3.id,
+                    'answer': [self.c33.id, self.c34.id]
+                },
+                {
+                    'question_id': self.q4.id,
                     'answer': 34
+                },
+            ]
+        }
+         
+        result = save_response(data)
+
+        self.assertFalse(result)
+
+        self.assertEqual(Response.objects.count(), 0)
+        self.assertEqual(QuestionResponse.objects.count(), 0)
+
+
+    def test_save_response_checkbox_with_int_answer(self):
+        """
+        save_response() should fail when the answer to a
+        CheckboxQuestion is given as an integer, and not a list (just
+        an int is the answer for a SliderQuestion). This means it
+        should return false and no response object should be saved in
+        the database.
+        """
+
+        data = {
+            'quiz_id': self.quiz.user_id,
+            'name': 'Benjamin',
+            'emoji': '😀',
+            'background_color': 0x333333,
+            'questions': [
+                {
+                    'question_id': self.q1.id,
+                    'answer': 2
+                },
+                {
+                    'question_id': self.q2.id,
+                    'answer': [self.c21.id, self.c23.id]
+                },
+                {
+                    'question_id': self.q3.id,
+                    'answer': [self.c33.id, self.c34.id]
+                },
+                {
+                    'question_id': self.q4.id,
+                    'answer': 34
+                },
+            ]
+        }
+         
+        result = save_response(data)
+
+        self.assertFalse(result)
+
+        self.assertEqual(Response.objects.count(), 0)
+        self.assertEqual(QuestionResponse.objects.count(), 0)
+
+
+    def test_save_response_slider_with_list_answer(self):
+        """
+        save_response() should fail when the answer to a SliderQuestion
+        is given as a list, and not an integer (a list is the answer
+        for a CheckboxQuestion). This means it should return false and
+        no response object should be saved in the database.
+        """
+
+        data = {
+            'quiz_id': self.quiz.user_id,
+            'name': 'Benjamin',
+            'emoji': '😀',
+            'background_color': 0x333333,
+            'questions': [
+                {
+                    'question_id': self.q1.id,
+                    'answer': [self.c11.id]
+                },
+                {
+                    'question_id': self.q2.id,
+                    'answer': [self.c21.id, self.c23.id]
+                },
+                {
+                    'question_id': self.q3.id,
+                    'answer': [self.c33.id, self.c34.id]
+                },
+                {
+                    'question_id': self.q4.id,
+                    'answer': [-23, 24]
+                },
+            ]
+        }
+         
+        result = save_response(data)
+
+        self.assertFalse(result)
+
+        self.assertEqual(Response.objects.count(), 0)
+        self.assertEqual(QuestionResponse.objects.count(), 0)
+
+
+    def test_save_response_multiple_choices_single_select(self):
+        """
+        save_response() should fail when multiple selected choices
+        are given for a CheckboxQuestion that is marked as
+        single-select.  This means it should return false and no
+        response object should be saved in the database.
+        """
+
+        data = {
+            'quiz_id': self.quiz.user_id,
+            'name': 'Benjamin',
+            'emoji': '😀',
+            'background_color': 0x333333,
+            'questions': [
+                {
+                    'question_id': self.q1.id,
+                    'answer': [self.c11.id, self.c12.id]
+                },
+                {
+                    'question_id': self.q2.id,
+                    'answer': [self.c21.id, self.c23.id]
                 },
             ]
         }
